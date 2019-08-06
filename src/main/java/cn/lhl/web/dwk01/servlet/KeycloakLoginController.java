1f3f7007f192e86ff95c50b66ee3220774bcc00c
liuhongliang <liuhongliang@itcast.cn>
2019-08-06 10:53:39 +0800

    dev-ems-01:整合v1.1基本完成

--- src/main/java/cn/lhl/web/dwk01/servlet/KeycloakLoginController.java
+++ src/main/java/cn/lhl/web/dwk01/servlet/KeycloakLoginController.java
@@ -18,6 +18,25 @@ import java.util.List;
  */
 @WebServlet("/keycloak/login")
 public class KeycloakLoginController extends HttpServlet {
+
+    /*
+    ems 与 keycloak 整合 过渡期方案
+    1. ems 新建一个controller 参考本类 此类的逻辑用于将keycloak用户转换为ems用户 并模拟原ems登录逻辑进行自动登录
+    2. 将此controller加入到原来登录filter的白名单中 不过滤
+    3. 将此controller配置到keycloak filter中
+
+    这样就可以实现以下过渡期要求
+    允许两套账号体系并存 依然可以使用原ems账号从原来登录页面登录
+    如果从别的系统已经在keycloak单点登录了 跳转到ems时能自动登录
+
+    但是使用时要注意
+    1. 从别的系统跳转到ems时 入口地址必须是 新建的controller
+    2. ems中的用户是keycloak用户的一个子集 对于keycloak有 ems没有的用户 要考虑提示用户 参考传智大学就业系统的实现方案
+    3. 用户 lhl@itcast.cn 在ems使用zixun01登录
+      后来在其他系统跳转回ems时 由于有一步自动登录的操作 所以zixun01可能会变成lhl@itcast.cn
+    4. 退出时只能退出ems 不是单点退出
+     */
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String prefix = "/keycloak/login";
